50b7bae53672b5643753786f8d898e84e9e614e4
Balsama <balsama@example.com>
2026-08-18 07:11:06 +0300

    Ajout du plugin Netlify pour TanStack Start

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "server" },
     }),
+    netlify(),
     viteReact(),
     tailwindcss(),
   ],
